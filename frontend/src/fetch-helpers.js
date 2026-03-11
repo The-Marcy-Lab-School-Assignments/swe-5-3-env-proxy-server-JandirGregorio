628e9a7ad3d7848403e67a4fc4f1d28e3d45fa8f
Jandir <jandirgregorio@icloud.com>
2026-03-11 15:55:40 -0400

    added link to Render

--- frontend/src/fetch-helpers.js
+++ frontend/src/fetch-helpers.js
@@ -19,7 +19,7 @@ export const getTrendingGifs = async () => {
 
 export const getGifsBySearch = async (searchTerm) => {
   try {
-    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=3`);
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchTerm}&limit=3`);
 
     if (!response.ok) {
       throw new Error(`Fetch failed with status - ${response.status}, ${response.statusText}`);
