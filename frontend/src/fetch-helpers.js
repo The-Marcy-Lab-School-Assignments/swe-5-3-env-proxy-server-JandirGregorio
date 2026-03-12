5933336c05efeaf23ef58cfd961e444df8a9bdf2
Jandir <jandirgregorio@icloud.com>
2026-03-12 09:05:05 -0400

    updated getGifsBySearch to fetch correctly

--- frontend/src/fetch-helpers.js
+++ frontend/src/fetch-helpers.js
@@ -14,3 +14,20 @@ export const getTrendingGifs = async () => {
     return { data: null, error };
   }
 };
+
+export const getGifsBySearch = async (searchTerm) => {
+  try {
+    const response = await fetch(`/api/gifs?q=${searchTerm}`);
+
+    if (!response.ok) {
+      throw new Error(`Fetch failed with status - ${response.status}, ${response.statusText}`);
+    }
+
+    const responseData = await response.json();
+
+    return { data: responseData, error: null };
+  } catch (error) {
+    console.warn(error);
+    return { data: null, error };
+  }
+};
